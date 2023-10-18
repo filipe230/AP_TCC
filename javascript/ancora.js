31e1a05844724c05f3afdf9664c1f8eb95042157
alvesnicole <96265666+alvesnicole@users.noreply.github.com>
2023-10-18 13:35:40 -0300

    Update ancora.js

--- javascript/ancora.js
+++ javascript/ancora.js
@@ -1,22 +1,18 @@
 var url_programiz = "https://www.programiz.com/python-programming/online-compiler/";
-var url_aula1 = "aula1.html";
-var url_aula2 = "aula2.html";
-var url_aula_operadores = "aula_operadores.html";
-var url_aula3 = "aula3.html";
-var url_aula4 = "aula4.html";
-var url_aula6 = "aula6.html";
-var url_ENATI = "https://enati-ifgoiano.github.io/2022/#/";
-var url_COLAB = "https://colab.research.google.com/drive/1iXwiC_NSjX8aWgnP6AKKca0xyEtK2n5b";
+var url_aula1 = "https://colab.research.google.com/drive/1iXwiC_NSjX8aWgnP6AKKca0xyEtK2n5b?authuser=3#scrollTo=_C74RhohyFln";
+var url_aula2 = "https://colab.research.google.com/drive/1yN10qHovel4Hb0H672sauD9Rm4UQA6wr?authuser=3";
+var url_aula3 = "https://colab.research.google.com/drive/1JjlnMA_YQ35gpyuec_yGXuwjLLzlXtS1?authuser=3";
+var url_aula4 = "https://colab.research.google.com/drive/1oRpjlPbSlS04UYLaeudH6zRGt7DY1vmu?authuser=3";
+var url_aula5 = "";
+var url_jogos = "";
 
 var programiz = document.getElementById("card-programiz");
-var enati = document.getElementById("card-enati");
 var aula1 = document.getElementById("card-aula1");
 var aula2 = document.getElementById("card-aula2");
-var aula_operadores = document.getElementById("card-aula-operadores");
 var aula3 = document.getElementById("card-aula3");
 var aula4 = document.getElementById("card-aula4");
-var aula6 = document.getElementById("card-aula6");
-var colab = document.getElementById("card-colab")
+var aula5 = document.getElementById("card-aula5");
+var jogos = document.getElementById("card-jogos")
 
 programiz.addEventListener('click', function() {
     
@@ -24,15 +20,9 @@ programiz.addEventListener('click', function() {
   
 });
 
-colab.addEventListener('click', function() {
+jogos.addEventListener('click', function() {
     
-    openInNewTab(url_COLAB);
-  
-});
-
-enati.addEventListener('click', function() {
-    
-    openInNewTab(url_ENATI);
+    openInNewTab(url_jogos);
   
 });
 
@@ -48,13 +38,6 @@ aula2.addEventListener('click', function() {
   
 });
 
-aula_operadores.addEventListener('click', function() {
-    
-    openNextPage(url_aula_operadores);
-  
-});
-
-
 aula3.addEventListener('click', function() {
     
     openNextPage(url_aula3);
@@ -67,9 +50,9 @@ aula4.addEventListener('click', function() {
   
 });
 
-aula6.addEventListener('click', function() {
+aula5.addEventListener('click', function() {
     
-    openNextPage(url_aula6);
+    openNextPage(url_aula5);
   
 });
 
@@ -83,7 +66,7 @@ function openInNewTab(url) {
 function openNextPage(url) {
     var janela = window.open(url, '_parent');
     janela.focus();
-}        
+}
 
 
 
