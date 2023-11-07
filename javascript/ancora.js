f69e49878c4f8dc5172ea7a8d725f8467f9748a6
filipe230 <filipeportilho77@gmail.com>
2023-11-07 17:35:06 -0300

    tutores e plano de aula 07.11

--- javascript/ancora.js
+++ javascript/ancora.js
@@ -6,13 +6,31 @@ var url_aula4 = "https://colab.research.google.com/drive/1NsJlDbv13CNC-8u3YZaUd2
 var url_aula5 = "";
 var url_jogos = "";
 
+/*var plano_aula1 = "";
+var plano_aula2 = "";
+var plano_aula3 = "";
+var plano_aula4 = "";
+var plano_aula5 = "";
+var plano_aula6 = "";
+var plano_aula7 = "";*/
+
 var programiz = document.getElementById("card-programiz");
 var aula1 = document.getElementById("card-aula1");
 var aula2 = document.getElementById("card-aula2");
 var aula3 = document.getElementById("card-aula3");
 var aula4 = document.getElementById("card-aula4");
 var aula5 = document.getElementById("card-aula5");
-var jogos = document.getElementById("card-jogos")
+var jogos = document.getElementById("card-jogos");
+
+/*var plano_1 = document.getElementById("card-aula1");
+var plano_2 = document.getElementById("card-aula2");
+var plano_3 = document.getElementById("card-aula3");
+var plano_4 = document.getElementById("card-aula4");
+var plano_5 = document.getElementById("card-aula5");
+var plano_6 = document.getElementById("card-jogos");
+var plano_7 = document.getElementById("card-jogos");*/
+
+
 
 programiz.addEventListener('click', function() {
     
